80559837cd0c0fdcb8cdd8690d20120a59f63558
Deide <deide@live.co.uk>
2019-07-29 18:25:46 +0100

    Add and update more emulators + games

--- install-scripts/uplay.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item "$dir\`$PLUGINSDIR" -Force -Recurse
-$folder = "cache\installers\"
-if (-not (Test-Path "$persist_dir\$folder")) {
-    Write-Host 'No' $folder 'folder exists. Creating.' -f Yellow
-    New-Item "$dir\$folder" -Type Directory -Force | Out-Null
-}
