bff12da2fefbb14b0abcbe76cfeadc32b8d4e80a
Deide <deide@live.co.uk>
2019-12-02 07:18:28 +0000

    Rework auto-pr to allow multiple apps to be specified
    Requires upstream changes.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,10 +1,16 @@
 param(
     # overwrite upstream param
-    [String] $Upstream = "deide/deide-bucket:master",
-    [Alias("App")][String[]] $Apps = ("*")
+    [Alias("Repo")][String] $Upstream = "deide/deide-bucket:master",
+    [Alias("App")][String[]] $Apps = ("*"),
+    [Switch] $Request,
+    [Switch] $Push
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
-Invoke-Expression -command "$autopr -dir $dir -upstream $Upstream -App $($Apps | ForEach-Object { "$_ " })"
+if ($PullRequest.IsPresent -and $Push.IsPresent) {
+    Write-Error "Must supply push (-p) or pull request (-r) switches, but not both at the same time";
+    exit;
+}
+$autopr = "$env:SCOOP_HOME\\bin\\auto-pr.ps1"
+$dir = "$psscriptroot\\..\\bucket" # checks the parent dir
+& $autopr -Dir $dir -Upstream $Upstream -Request:$Request -Push:$Push -App ($Apps -Join " ")
