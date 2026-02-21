67b67b66387449449f0603675baf516f304495a7
Deide <deide@live.co.uk>
2026-02-21 23:16:30 +0000

    Update auto-pr

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,16 +1,9 @@
 param(
     # overwrite upstream param
-    [Alias("Repo")][String] $Upstream = "deide/deide-bucket:master",
-    [Alias("App")][String[]] $Apps = ("*"),
-    [Switch] $Request,
-    [Switch] $Push
+    [String]$upstream = "ScoopInstaller/Main:master"
 )
 
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-if ($PullRequest.IsPresent -and $Push.IsPresent) {
-    Write-Error "Must supply push (-p) or pull request (-r) switches, but not both at the same time";
-    exit;
-}
-$autopr = "$env:SCOOP_HOME\\bin\\auto-pr.ps1"
-$dir = "$psscriptroot\\..\\bucket" # checks the parent dir
-& $autopr -Dir $dir -Upstream $Upstream -Request:$Request -Push:$Push -App ($Apps -Join " ")
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
+Invoke-Expression -Command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
